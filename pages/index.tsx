564f706e0ec3d90e45a2862f4936a929b59a98cc
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2021-08-02 16:49:39 -0300

    Configuração das pastas e adição do typescript
    Co-authored-by: Bruno Corrêa <correaricardobruno@gmail.com>

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Title</title>
+      </Head>
+
+      <main>
+        <h1>Hello E-Learning!</h1>
+      </main>
+    </div>
+  )
+}
