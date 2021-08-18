f6bb7b8cca3b5aa740ea4a9f6f864a8720b93b4d
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2021-08-17 21:21:57 -0300

    Testando o compartilhamento de estado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,11 +2,12 @@ import { useContext } from "react"
 import { AuthContext } from "../../contexts/AuthContext"
 
 export default function Header(){
-    const { signOut } = useContext(AuthContext)
+    const { signOut, user } = useContext(AuthContext)
 
     return (
         <div>
             <button onClick={signOut}>Logout</button>
+            <img src={user?.foto} alt={user?.nome} width="50px" />
         </div>
     )
 }
